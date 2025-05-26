bc9ccbc3331db4ff3adfda151dbcc24f53e3bd82
Shlok Bam <shlokbam19103@gmail.com>
2025-05-27 01:16:41 +0530

    updates for deployment

--- app.py
+++ app.py
@@ -66,4 +66,5 @@ def view_hackathon_certificate(filename):
         return jsonify({'success': False, 'message': 'Certificate not found'}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port) 
