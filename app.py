0bb010b413806cbd9fa36a9d2880fd50eb2ab43c
Shlok Bam <shlok.bam23@vit.edu>
2026-05-03 10:36:46 +0530

    style: improve mobile responsive layout and enable debug mode in development server

--- app.py
+++ app.py
@@ -195,4 +195,4 @@ def view_hackathon_certificate(filename):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
